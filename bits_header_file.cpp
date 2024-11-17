ba98897f63874ac08cb4ba6bd02decef0ccdd4ab
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-18 00:08:06 +0600

    bits_header_file

---
+++ bits_header_file.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h> /// ai header filer moddei sob header file ache alada alada kore onkgula lekhar dorkar hoy na r
+using namespace std;
+
+int main()
+{
+    cout << "hello" << endl;
+}
