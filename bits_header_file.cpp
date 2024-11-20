981e767ba0e91b83cb87259a6391f932894bb476
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-20 21:39:32 +0600

    dynamic_array_size_increase

--- bits_header_file.cpp
+++ bits_header_file.cpp
@@ -3,5 +3,37 @@ using namespace std;
 
 int main()
 {
-    cout << "hello" << endl;
+    int n;
+    cin >> n;
+
+    int *arrA = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arrA[i];
+    }
+
+    int m;
+    cin >> m;
+    int *arrB = new int[m];
+
+    for (int i = 0; i < n; i++)
+    {
+        arrB[i] = arrA[i];
+    }
+
+    for (int i = n; i < m; i++)
+    {
+        cin >> arrB[i];
+    }
+
+    delete[] arrA;
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << arrB[i] << " ";
+    }
+
+    delete[] arrB;
+    return 0;
 }
