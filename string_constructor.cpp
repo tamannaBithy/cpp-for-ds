edc8048befb2d888be27fdeded096a4a9bb6e066
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-23 20:44:31 +0600

    recursive_stringstream

---
+++ string_constructor.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+///////////////// different operation on a string
+
+// int main()
+// {
+//     // //1
+//     // string s = "hello world";
+//     // // 2
+//     // string s("hello world");
+//     // // 3
+//     // string s("hello world", 7);
+//     // //4
+//     // string a = "hello world";
+//     // string s(a, 4);
+//     // 5
+//     string s(26, 'a');
+//     cout << s << endl;
+//     return 0;
+// }
+
+//////////////////////// sorting
+
+// int main(){
+//     string s;
+//     cin >> s;
+//     sort(s.begin(), s.end());
+//     cout << s << endl;
+//     return 0;
+// }
+
+//////////////////// range based for loop
+
+// int main()
+// {
+//     string s;
+//     cin >> s;
+//     for (char c : s)
+//     {
+//         cout << s << endl;
+//     }
+//     //// aivabe sudu matro element pawa jay, index pawa jay na. index dorkar hole normal for loop use korte hbe
+//     return 0;
+// }
--- stringstream.cpp
+++ stringstream.cpp
@@ -1,21 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/// streamstring holo akta sentencer modde sobgula word alada alada vabe access korte parar akta constructor
+
+// int main()
+// {
+//     string s;
+//     getline(cin, s);
+//     stringstream ss(s);
+//     // ss << s; // streamstringer modde s na diye aivabe o kora jay
+//     string word;
+//     int count = 0;
+//     while (ss >> word)
+//     {
+//         cout << word << endl;
+//         count++;
+//     }
+//     cout << count << endl;
+//     return 0;
+// }
+
+//////// recursive way te ba ulta side theke word gula print korar jonno
+
+void print_recursive(stringstream &ss)
+{
+    string word;
+    if (ss >> word)
+    {
+        print_recursive(ss);
+        cout << word << endl;
+    }
+}
+
 int main()
 {
     string s;
     getline(cin, s);
     stringstream ss(s);
-    // ss << s; // streamstringer modde s na diye aivabe o kora jay
-    string word;
-    int count = 0;
-    while (ss >> word)
-    {
-        cout << word << endl;
-        count++;
-    }
-    cout << count << endl;
+    print_recursive(ss);
     return 0;
 }
-
-/// streamstring holo akta sentencer modde sobgula word alada alada vabe access korte parar akta constructor
