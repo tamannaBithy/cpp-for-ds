1bbaa9efda1c319438ab09cda36baa4811a803c9
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-11-28 21:39:08 +0600

    B_ICPC Balloons

--- B_ICPC_Balloons.cpp
+++ B_ICPC_Balloons.cpp
@@ -11,33 +11,25 @@ int main()
     {
         int n;
         cin >> n;
-        char s[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> s[i];
-        }
+        string s;
+        cin >> s;
 
         int freq[26] = {0};
+        int result = 0;
 
-        for (int i = 0; i < n; i++)
+        for (char c : s)
         {
-            int value = s[i] - 'A';
+            int value = c - 'A';
 
             if (freq[value] == 0)
             {
-                freq[value] += 2;
+                result += 2;
             }
             else
             {
-                freq[value]++;
+                result++;
             }
-        }
-
-        int result = 0;
-
-        for (int i = 0; i < 26; i++)
-        {
-            result += freq[i];
+            freq[value]++;
         }
 
         cout << result << endl;
