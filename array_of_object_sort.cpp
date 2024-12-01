4c978782845e3712a2f3d1e40f66284f795846f3
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2024-12-01 20:36:38 +0600

    hackerrank_problem_solved

--- array_of_object_sort.cpp
+++ array_of_object_sort.cpp
@@ -40,3 +40,42 @@ int main()
 
     return 0;
 }
+
+/////// swaping based on section
+
+// class Student
+// {
+// public:
+//   string name;
+//   int cls;
+//   char sec;
+//   int id;
+// };
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+//   Student a[n];
+//   for (int i = 0; i < n; i++)
+//   {
+//     cin >> a[i].name >> a[i].cls >> a[i].sec >> a[i].id;
+//   }
+
+//   int i = 0;
+//   int j = n - 1;
+//   while (i < j)
+//   {
+//     swap(a[i].sec, a[j].sec);
+//     i++;
+//     j--;
+//   }
+
+//   for (int i = 0; i < n; i++)
+//   {
+
+//     cout << a[i].name << " " << a[i].cls << " " << a[i].sec << " " << a[i].id << endl;
+//   }
+
+//   return 0;
+// }
--- hello.cpp
+++ hello.cpp
@@ -1,25 +1,59 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+// int main()
+// {
+//   // cout << "hello world";
+
+//   int a = 10;
+//   int b = 1000000;
+
+//   // cout << a << endl
+//   //      << b;
+
+//   //////// taking input & typecasting (data type change kora)
+
+//   char c;
+//   cin >> c;
+
+//   cout << int(c) << endl;
+
+//   int d = 100;
+//   long long int p = (long long int)(d); // jodi akadik worder typer nam hoy tahle aivabe convert kora lage
+
+//   return 0;
+// }
+
+////////////  find a specific word from a string & replace it with "#"
 
 int main()
 {
-  // cout << "hello world";
+  int t;
+  cin >> t;
 
-  int a = 10;
-  int b = 1000000;
+  for (int j = 0; j < t; j++)
+  {
+    string s;
+    cin >> s;
+    string x;
+    cin >> x;
 
-  // cout << a << endl
-  //      << b;
+    int xLength = x.length();
 
-  //////// taking input & typecasting (data type change kora)
+    string result = "";
+    for (int i = 0; i < s.length(); i++)
+    {
+      if (i + xLength - 1 < s.length() && s.substr(i, xLength) == x)
+      {
+        result += "#";
+        i += xLength - 1;
+      }
+      else
+      {
+        result += s[i];
+      }
+    }
 
-  char c;
-  cin >> c;
-
-  cout << int(c) << endl;
-
-  int d = 100;
-  long long int p = (long long int)(d); // jodi akadik worder typer nam hoy tahle aivabe convert kora lage
-
-  return 0;
+    cout << result << endl;
+  }
 }
